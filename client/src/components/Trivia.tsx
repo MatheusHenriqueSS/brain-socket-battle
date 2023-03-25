2c26a0eb5557296b5281a8d529c09f30c95b83ea
Matheus Henrique <matheushss2013@gmail.com>
2023-03-25 19:12:36 -0300

    feat(front): format chat messages

--- client/src/components/Trivia.tsx
+++ client/src/components/Trivia.tsx
@@ -1,16 +1,40 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-
-function GetUsername() {
-}
+import { io } from "socket.io-client";
+import { socket } from "../socket";
 
 export function Trivia() {
     const [searchParams, setSearchParams] = useSearchParams();
     const [username, setUsername] = useState("");
+    const [chatMessages, setChatMessages] = useState(new Array<{playerName: string, text: string, createdAt: string}>());
+    const roomRef = useRef("");
 
-    useEffect(() => {
-        setUsername(searchParams.get("playerName") || "");
+    const initializedRef = useRef(false);
+
+    const initializeValues = useCallback(() => {
+        if(!initializedRef.current) {
+            const currentUsername = searchParams.get("playerName") || ""; 
+            setUsername(currentUsername);
+            roomRef.current = searchParams.get("room") || "";
+            socket.emit('join', {playerName: currentUsername, room: roomRef.current}, (err: any) => {
+                if(err) {
+                    alert(err);
+        
+                }
+            });
+
+            initializedRef.current = true;
+        }
     }, []);
+
+    socket.on('message', ({ playerName, text, createdAt}) => {
+        setChatMessages([...chatMessages, {playerName, text, createdAt}]);
+    })
+    
+    useEffect(() => {
+        initializeValues();
+    }, [initializeValues]);
+
 
     return (
         <main>
@@ -43,7 +67,23 @@ export function Trivia() {
 
             <section className="section chat">
                 <h2 className="subheading">chat</h2>
-                <div className="chat__messages"></div>
+                <div className="chat__messages">
+                    {
+                        chatMessages.map((message) => {
+                            return (    
+                                <div className="message">
+                                    <p>
+                                        <span className="message__playername"> {message.playerName} </span>
+                                        <span className="message__meta"> {new Date(message.createdAt).toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true })} </span>
+                                    </p>
+                                    <p>
+                                        {message.text}
+                                    </p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
                 <form className="chat__form">
                 <input
                     className="form__input chat__message"
